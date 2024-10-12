eba6f5305d435a180509ce0d67e24ab7491397f8
linguirezz <alinuriman000@gmail.com>
2024-10-12 12:38:45 +0700

    nav and footer

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#4A4947'
+      }
+    },
+  },
+  plugins: [],
+}
